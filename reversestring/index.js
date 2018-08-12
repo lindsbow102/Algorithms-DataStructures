670c920c7d9fd9cfd44af8a8af926cf1a827dbaa
Lindsey Bowen <lindseybwn@gmail.com>
2018-08-11 18:29:03 -0700

    More ReverseString options

--- reversestring/index.js
+++ reversestring/index.js
@@ -6,15 +6,38 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
+//Option 3 -- still trying to make sense of this one
+const reverse = (str) => {
+    
+        let reversed = '';    
+        for (let character of str) {
+            reversed = character + reversed;
+        }    
+        return reversed;
+    };
+
+module.exports = reverse;
 
 //Option 1
 // const reverse = (str) => {
-//     return str.split('').reverse().join(''); 
+//     return str.split('').reverse().join('');
 // };
 
 //Option 2 -- more modern using backticks
-const reverse = (str) => {
-    return str.split``.reverse().join``; 
-};
+// const reverse = str => {
+//     return str.split``.reverse().join``;
+//   };
 
-module.exports = reverse;
+//Option 3 -- still trying to make sense of this one
+// const reverse = (str) => {
+    //     let reversed = '';    
+    //     for (let character of str) {
+    //         reversed = character + reversed;
+    //     }    
+    //     return reversed;
+    // };
+
+//Option 4
+//     const reverse = (str) => (
+//     str.split``.reduce((reversed, character) => character + reversed, '')
+// );
