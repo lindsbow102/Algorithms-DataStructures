4e2ad74048e8604e94b5ccd3be546bcf859754f6
Lindsey Bowen <lindseybwn@gmail.com>
2018-08-13 17:02:26 -0700

    reverse integers

---
+++ reverseint/index.js
@@ -0,0 +1,46 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+
+const reverseInt = (n)  => {
+    const str = n.toString();
+    const reversed = str.split``.reverse().join``;
+    const reversedN = parseInt(reversed);
+    return Math.sign(n) * reversedN;
+};
+
+    
+
+module.exports = reverseInt;
+
+// Option 1 -- MY solution
+// const reverseInt = (n)  => {
+//     const str = n.toString();
+//     const reversed = str.split``.reverse().join``;
+//     return n < 0 ? 0 - parseInt(reversed) : parseInt(reversed);
+// };
+
+// Option 2 -- also mine
+// const reverseInt = (n)  => {
+//     const str = n.toString();
+//     const reversed = str.split``.reverse().join``;
+//     const reversedN = parseInt(reversed);
+//     return Math.sign(n) * reversedN;
+// };
+
+//Option 3 -- Stephen's
+// const reverseInt = n => {
+//     const reversed = n.toString().split``.reverse().join``;
+//     if (n < 0) {
+//         return parseInt(reversed) * -1
+//     } else {
+//         return parseInt(reversed);
+//     }
+// };
