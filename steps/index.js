24531ca92a53e718965854947fffe6d23388277e
Lindsey Bowen <lindseybwn@gmail.com>
2018-08-26 11:55:29 -0700

    Added recursive solution to steps

--- steps/index.js
+++ steps/index.js
@@ -17,19 +17,40 @@
 //       '### '
 //       '####'
 
-const steps = (n) => {
-    for(let row = 0; row < n; row ++) {
-        let stair = '';
+// Recursion
+const steps = (n, row = 0, stair = '') => {
+    if (row === n) {
+        return;
+    }
 
-        for(let column = 0; column < n; column++) {
-            if (column <= row) {
-                stair += '#';
-            } else {
-                stair += ' ';
-            }
-        }
+    if (stair.length === n) {
         console.log(stair);
-    }    
-}
+        return steps(n, row + 1);
+    } 
+    
+    if (stair.length <= row) {
+        stair += '#';
+    } else {
+        stair += ' ';
+    }
+
+    steps(n, row, stair);
+};
 
 module.exports = steps;
+
+// Iterative solution --easier to read/understand and recommended for interview setting
+// const steps = (n) => {
+//     for(let row = 0; row < n; row ++) {
+//         let stair = '';
+
+//         for(let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#';
+//             } else {
+//                 stair += ' ';
+//             }
+//         }
+//         console.log(stair);
+//     }    
+// }
